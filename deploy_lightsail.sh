252921a94f519ac0bba21e86b1cd160a786ee05a
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-25 23:31:30 -0400

    generic server name

--- deploy_lightsail.sh
+++ deploy_lightsail.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-SERVER_NAME=${1:-"HarshDoorstopServer"}
+SERVER_NAME=SteamServer
 
 
 aws lightsail create-instances \
@@ -9,6 +9,9 @@ aws lightsail create-instances \
     --blueprint-id amazon_linux_2 \
     --bundle-id nano_2_0 \
     --user-data file://lightsail_startup.sh
+
+aws lightsail wait instance-running --instance-name $SERVER_NAME --max-wait-time-seconds 900
+
 aws lightsail open-instance-public-ports --instance-name $SERVER_NAME \
     --port-info fromPort=7777,toPort=7777,protocol=udp \
     --port-info fromPort=7779,toPort=7779,protocol=tcp \
