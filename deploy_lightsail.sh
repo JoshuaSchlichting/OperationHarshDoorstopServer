fb39b87878a2a49455a0e6b5d9aacac0843d6752
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-25 20:19:20 -0400

    availabiltiy zone

--- deploy_lightsail.sh
+++ deploy_lightsail.sh
@@ -5,7 +5,7 @@ SERVER_NAME=${1:-"HarshDoorstopServer"}
 
 aws lightsail create-instances \
     --instance-name $SERVER_NAME \
-    --availability-zone us-east-1 \
+    --availability-zone us-east-1a \
     --blueprint-id amazon_linux_2 \
     --bundle-id nano_2_0 \
     --user-data file://lightsail_startup.sh
