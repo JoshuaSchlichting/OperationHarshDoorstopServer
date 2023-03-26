c44055a8f19aa2bc5986b5e278d0643bb1a4e871
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-26 16:00:44 -0400

    rm findreplace

--- counterstrike/run.sh
+++ counterstrike/run.sh
@@ -1,8 +1,5 @@
 docker build -t ohd .
 
-findreplace -find "Counter-Strike 1.6 Server" -replace "$1" -file server.cfg
-findreplace -find "rconpassword" -replace "$2" -file server.cfg
-
 docker run \
     --name counter-strike_server \
     -p 27015:27015/udp \
