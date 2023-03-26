0ca60e37eb3da3179241ea319162bc2be5f1d3d5
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-25 23:27:32 -0400

    region work

--- deploy_lightsail.sh
+++ deploy_lightsail.sh
@@ -2,8 +2,6 @@
 
 SERVER_NAME=${1:-"HarshDoorstopServer"}
 
-aws configure set region us-east-1
-export AWS_DEFAULT_REGION=us-east-1
 
 aws lightsail create-instances \
     --instance-name $SERVER_NAME \
