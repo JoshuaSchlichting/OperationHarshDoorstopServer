3eb99e07036d2ca76f28a1a85581924a9054e8a4
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-26 16:22:10 -0400

    fix mount

--- counterstrike/run.sh
+++ counterstrike/run.sh
@@ -4,5 +4,5 @@ docker run \
     --name counter-strike_server \
     -p 27015:27015/udp \
     -p 27015:27015 \
-    -v server.cfg:/hlds/cstrike/server.cfg \
+    -v $(pwd)/server.cfg:/hlds/cstrike/server.cfg \
     febley/counter-strike_server:latest
