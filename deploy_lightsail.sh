05f6b75282f87aa28e84832c666973ba8a57944f
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2023-03-25 20:11:54 -0400

    separate port cmd

--- deploy_lightsail.sh
+++ deploy_lightsail.sh
@@ -9,6 +9,7 @@ aws lightsail create-instances \
     --blueprint-id amazon_linux_2 \
     --bundle-id nano_2_0 \
     --user-data file://lightsail_startup.sh
+aws lightsail open-instance-public-ports --instance-name $SERVER_NAME \
     --port-info fromPort=7777,toPort=7777,protocol=udp \
     --port-info fromPort=7779,toPort=7779,protocol=tcp \
     --port-info fromPort=27005,toPort=27005,protocol=udp
